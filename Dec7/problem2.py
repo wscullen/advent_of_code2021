6b75198c1ddb39559a4adfd2f4ad65e0f9464d43
Shaun Cullen <shaun@cullen.io>
2021-12-17 23:51:08 -0700

    Day 7 problems 1 and 2

--- Dec7/problem2.py
+++ Dec7/problem2.py
@@ -1,4 +1,5 @@
 from typing import List, Union, MutableSet
+import math
 
 from enum import Enum
 
@@ -9,11 +10,16 @@ with open("./input.txt") as reader:
     max_position = max(crab_positions)
     print(crab_positions)
     print(max_position)
+
+    print()
     sums = []
     for index in range(0, max_position):
         current_sum = 0
         for val in crab_positions:
-            current_sum += abs(val - index)
+            sub_sum = 0
+            for sub_index in range(1, abs(val - index) + 1):
+                sub_sum += sub_index
+            current_sum += sub_sum
 
         sums.append(current_sum)
 
